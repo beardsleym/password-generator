3e90ae5b9ade901ab2cc88b5161c55ac2149d0a4
Matthew Beardsley <matthew@beardsley.com.au>
2021-09-15 02:50:29 +0800

    ipdata font xs

--- src/IpData.js
+++ src/IpData.js
@@ -1,9 +1,9 @@
 const IpData = ({ipData}) => {
   return (  
     <div className="flex">
-      <div className="text-gray-600 font-extralight flex-1 text-center font-mono text-sm">ip: {ipData.ip}</div>
-      <div className="text-gray-600 font-extralight flex-1 text-center font-mono text-sm">country: {ipData.country}</div>
-      <div className="text-gray-600 font-extralight flex-1 text-center font-mono text-sm">platform: {ipData.platform}</div>
+      <div className="text-gray-600 font-extralight flex-1 text-center font-mono text-xs">ip: {ipData.ip}</div>
+      <div className="text-gray-600 font-extralight flex-1 text-center font-mono text-xs">country: {ipData.country}</div>
+      <div className="text-gray-600 font-extralight flex-1 text-center font-mono text-xs">platform: {ipData.platform}</div>
     </div>
   );
 }
