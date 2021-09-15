0d5c6332abf85ac58a5516104beb6381cbde942f
Matthew Beardsley <matthew@beardsley.com.au>
2021-09-15 17:30:14 +0800

    hide city if no information from cf

--- src/IpData.js
+++ src/IpData.js
@@ -4,7 +4,7 @@ const IpData = ({ipData}) => {
   return (  
     <div className="flex">
       <div className="text-gray-600 font-extralight flex-grow text-center font-mono text-xs">{ipData.ip}<span className="hidden sm:inline">, {ipData.asOrganization}</span></div>
-      <div className="text-gray-600 font-extralight flex-grow text-center font-mono text-xs">{ipData.city}, {countryCodeEmoji(ipData.country)}</div>
+      <div className="text-gray-600 font-extralight flex-grow text-center font-mono text-xs">{ipData.city ? `${ipData.city}, ` : ''} {countryCodeEmoji(ipData.country)}</div>
       {/* <div className="text-gray-600 font-extralight flex-grow text-center font-mono text-xs">{ipData.platform}</div> */}
     </div>
   );
