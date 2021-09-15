a2fcb4a8f7774e9e180ec70c081e345f9ced08f0
Matthew Beardsley <matthew@beardsley.com.au>
2021-09-15 17:16:12 +0800

    hide asOrganization and feedback warning on mobile

--- src/IpData.js
+++ src/IpData.js
@@ -3,9 +3,9 @@ import { countryCodeEmoji } from "country-code-emoji";
 const IpData = ({ipData}) => {
   return (  
     <div className="flex">
-      <div className="text-gray-600 font-extralight flex-1 text-center font-mono text-xs">{ipData.ip}</div>
-      <div className="text-gray-600 font-extralight flex-1 text-center font-mono text-xs">{ipData.city}, {countryCodeEmoji(ipData.country)}</div>
-      <div className="text-gray-600 font-extralight flex-1 text-center font-mono text-xs">{ipData.platform}</div>
+      <div className="text-gray-600 font-extralight flex-grow text-center font-mono text-xs">{ipData.ip}<span className="hidden sm:inline">, {ipData.asOrganization}</span></div>
+      <div className="text-gray-600 font-extralight flex-grow text-center font-mono text-xs">{ipData.city}, {countryCodeEmoji(ipData.country)}</div>
+      {/* <div className="text-gray-600 font-extralight flex-grow text-center font-mono text-xs">{ipData.platform}</div> */}
     </div>
   );
 }
