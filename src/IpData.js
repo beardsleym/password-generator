9d5682b21fc9275086ae93393203040e6e105ab7
Matthew Beardsley <matthew@beardsley.com.au>
2021-09-18 16:35:08 +0800

    weather icon styling fix

--- src/IpData.js
+++ src/IpData.js
@@ -7,9 +7,9 @@ const IpData = ({ipData}) => {
 
   return (  
     <div className="flex">
-      <div className="text-gray-600 font-extralight flex-grow text-center font-mono text-xs"><span>{ipData.emoji}</span> {ipData.ip}<span className="hidden sm:inline">, {ipData.asOrganization}</span></div>
-      <div className="text-gray-600 font-extralight flex-grow text-center font-mono text-xs relative">{ipData.city ? `${ipData.city} ` : ''} {temp_string}<img className="img-fluid h-7 absolute -bottom-1.5 right-0" src={weatherIcon} alt={weatherAlt} /></div>
+      <div className="text-gray-600 font-extralight flex-grow text-center font-mono text-xs pt-1.5"><span>{ipData.emoji}</span> {ipData.ip}<span className="hidden sm:inline">, {ipData.asOrganization}</span></div>
       {/* <div className="text-gray-600 font-extralight flex-grow text-center font-mono text-xs">{ipData.platform}</div> */}
+      <div className="text-gray-600 font-extralight flex-grow text-center font-mono text-xs">{ipData.city ? `${ipData.city} ` : ''} {temp_string}<img className="h-7 inline-block" src={weatherIcon} alt={weatherAlt} /></div>
     </div>
   );
 }
