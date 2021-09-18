c6b37d053e8c380bb31e1734766f13f199a0c76e
Matthew Beardsley <matthew@beardsley.com.au>
2021-09-18 20:26:11 +0800

    ipdata component tests for weather and ipdata, cleaner look

--- src/IpData.js
+++ src/IpData.js
@@ -1,15 +1,19 @@
 const IpData = ({ipData}) => {
-  const weather = ipData.openweather
-  const weatherIcon = `https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`
-  const weatherAlt = weather.weather[0].description
-  const temp_string = weather.main.temp_string
+  let weatherIcon;
+  let weatherAlt;
+  let temp_string;
 
-
+  if(ipData.openweather) {
+    weatherIcon = `https://openweathermap.org/img/wn/${ipData.openweather.weather[0].icon}.png`|| null
+    weatherAlt = ipData.openweather.weather[0].description || null
+    temp_string = ipData.openweather.main.temp_string || ''
+  }
+  
   return (  
     <div className="flex">
-      <div className="text-gray-600 font-extralight flex-grow text-center font-mono text-xs pt-1.5"><span>{ipData.emoji}</span> {ipData.ip}<span className="hidden sm:inline">, {ipData.asOrganization}</span></div>
+      {ipData.ip && <div className="text-gray-600 font-extralight flex-grow text-center font-mono text-xs pt-1.5"><span>{ipData.emoji}</span> {ipData.ip}<span className="hidden sm:inline">, {ipData.asOrganization}</span></div>}
       {/* <div className="text-gray-600 font-extralight flex-grow text-center font-mono text-xs">{ipData.platform}</div> */}
-      <div className="text-gray-600 font-extralight flex-grow text-center font-mono text-xs">{ipData.city ? `${ipData.city} ` : ''} {temp_string}<img className="h-7 inline-block" src={weatherIcon} alt={weatherAlt} /></div>
+      {ipData.openweather && <div className="text-gray-600 font-extralight flex-grow text-center font-mono text-xs">{ipData.city ? `${ipData.city} ` : ''} {temp_string}<img className="h-7 inline-block" src={weatherIcon} alt={weatherAlt} /></div>}
     </div>
   );
 }
