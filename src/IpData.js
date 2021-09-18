fdb6b045c260990347910086c09f27d2d8d2e2cc
Matthew Beardsley <matthew@beardsley.com.au>
2021-09-18 15:18:30 +0800

    temp string imperial/metric returned from cf worker

--- src/IpData.js
+++ src/IpData.js
@@ -1,15 +1,14 @@
 const IpData = ({ipData}) => {
   const weather = ipData.openweather
   const weatherIcon = `https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`
-  const weatherAlt = weather.weather[0].main
-  const temp = `${weather.main.temp.toFixed(0)}°C`
+  const weatherAlt = weather.weather[0].description
+  const temp_string = weather.main.temp_string
 
 
   return (  
     <div className="flex">
-      <div className="text-gray-600 font-extralight flex-grow text-center font-mono text-xs">{ipData.ip}<span className="hidden sm:inline">, {ipData.asOrganization}</span></div>
-      <div className="text-gray-600 font-extralight flex-grow text-center font-mono text-xs">{ipData.city ? `${ipData.city} ` : ''}<span>{ipData.emoji}</span></div>
-      <div className="text-gray-600 font-extralight flex-grow text-center font-mono text-xs relative">{temp}<img className="hidden sm:inline img-fluid h-7 absolute -bottom-1 right-5" src={weatherIcon} alt={weatherAlt} /></div>
+      <div className="text-gray-600 font-extralight flex-grow text-center font-mono text-xs"><span>{ipData.emoji}</span> {ipData.ip}<span className="hidden sm:inline">, {ipData.asOrganization}</span></div>
+      <div className="text-gray-600 font-extralight flex-grow text-center font-mono text-xs relative">{ipData.city ? `${ipData.city} ` : ''} {temp_string}<img className="img-fluid h-7 absolute -bottom-1.5 right-0" src={weatherIcon} alt={weatherAlt} /></div>
       {/* <div className="text-gray-600 font-extralight flex-grow text-center font-mono text-xs">{ipData.platform}</div> */}
     </div>
   );
