2b20cdda4e57fdefdca90cda5e1e69260cc884bc
Matthew Beardsley <matthew@beardsley.com.au>
2021-10-10 20:54:46 +0800

    random word password option

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { RefreshIcon, ClipboardCopyIcon } from '@heroicons/react/outline'
 import zxcvbn from 'zxcvbn';
 import sha1 from 'crypto-js/sha1';
 import generator from 'generate-password';
+import niceware from 'niceware';
 // Components
 import IpData from './components/IpData';
 import Dividers from './components/Dividers';
@@ -26,28 +27,41 @@ function App() {
   const [uppercase, setUppercase] = useState(true);
   const [lowercase, setLowercase] = useState(true);
   const [excludeSimilarCharacters, setExcludeSimilarCharacters] = useState(false);
+  // Word Password - Niceware
+  const [passphrase, setPassphrase] = useState(false);
   // API Data
   const [isPwned, setIsPwned] = useState(0);
   const [ipData, setIpData] = useState(null);
   const [feedback, setFeedback] = useState('');
 
   const generatePassword = (value) => {
-    const config = {
-      length: value ? value : length,
-      numbers,
-      symbols,
-      lowercase,
-      uppercase,
-      excludeSimilarCharacters
-    }
-    try {
-      const result = generator.generate(config)
-      setPassword(result) 
-    } catch (error) {
-      console.log(error)
+    if (value === 'passphrase') {
+      setPassword(niceware.generatePassphrase(6).join('-'))
+    } else if (value === 'random') {
+      const config = {
+        length: 14,
+        numbers,
+        symbols,
+        lowercase,
+        uppercase,
+        excludeSimilarCharacters
+      }
+      setPassword(generator.generate(config)) 
+    } else if (passphrase) {
+      setPassword(niceware.generatePassphrase(value ? value * 2 : length * 2).join('-'))
+    } else {
+      const config = {
+        length: value ? value : length,
+        numbers,
+        symbols,
+        lowercase,
+        uppercase,
+        excludeSimilarCharacters
+      }
+      setPassword(generator.generate(config)) 
     }
   }
-
+  
   // Handle input changes
   const handleInput = (text) => {
     setPassword(text)
@@ -56,6 +70,17 @@ function App() {
 
   const handleClick = () => {
     generatePassword()
+  }
+
+  const handlePassphrase = (value) => {
+    if (value) {
+      setLength(3)
+      generatePassword('passphrase')
+    } else {
+      setLength(14)
+      generatePassword('random')
+    } 
+    setPassphrase(value)
   }
 
   const handleLength = (value) => {
@@ -149,6 +174,14 @@ function App() {
     generatePassword()
   },[uppercase,lowercase,symbols,numbers,radio])
 
+  // Generate a new word password if toggled
+  // useEffect(()=>{
+  //   setLength(3)
+  //   // setPassword(niceware.generatePassphrase(length))
+  //   console.log(niceware.generatePassphrase(length*2))
+  //   setPassword(niceware.generatePassphrase(length*2).join('-'))
+  // },[passphrase])
+
   // Check password strength
   useEffect(()=>{
     const result = zxcvbn(password)
@@ -194,11 +227,11 @@ function App() {
 
   // HTML
   return (
-    <div className={`App sm:h-screen w-full flex justify-center items-center bg-gradient-to-r from-gray-300 via-${color}-400 to-gray-300`}>
+    <div className={`App w-full flex justify-center bg-gradient-to-r from-gray-300 via-${color}-400 to-gray-300`}>
       <div className="w-full max-w-2xl" >
         <div className=" bg-white sm:rounded-3xl sm:shadow-lg px-1 sm:px-4 md:px-8 sm:py-8">
           <h1 className="text-2xl sm:text-4xl font-extrabold tracking-tight text-gray-900 text-center mb-1">Generate a secure password<span className={`text-6xl text-${color}-400`}>.</span></h1>
-            <h2 className="sm:text-lg text-gray-500 font-medium tracking-tight text-center mb-4">Or enter a <span className={`text-gray-900 cursor-pointer hover:text-${color}-400 transition duration-200 ease-in-out`} onClick={(event) => {handleInput('p@$$w0rd')}}>p@$$w0rd</span> to check its <span className={`text-gray-900 cursor-pointer hover:text-${color}-400 transition duration-200 ease-in-out`} onClick={(event) => {handleInput('strength')}}>strength</span><span className={`text-${color}-400 text-3xl`}>.</span></h2>
+            <h2 className="sm:text-lg text-gray-500 font-medium tracking-tight text-center mb-4">Or enter a <span className={`text-gray-900 cursor-pointer hover:text-${color}-400 transition duration-200 ease-in-out`} onClick={(event) => {handleInput('p@$$w0rd')}}>p@$$w0rd</span> to check its <span className={`text-gray-900 cursor-pointer hover:text-${color}-400 transition duration-200 ease-in-out`} onClick={(event) => {handleInput('strength')}}>strength</span><span className={`text-${color}-400 text-3xl`}>.</span></h2> 
             <div className="relative mb-20">
               <input type="text"  value={password} onChange={(event) => handleInput(event.target.value)} className="absolute tracking-wider border rounded w-full py-4 px-2 sm:px-5 text-gray-700 border-gray-300 focus:outline-none font-mono text-xl" />
                 <button onClick={handleCopyBtn} className="bg-white absolute top-2 right-11 p-2" type="button">
@@ -216,6 +249,31 @@ function App() {
             </div>
           {/* STRENGTH METER */}
           <StrengthMeter score={feedback.score} color={color}/>
+          {/* <!-- Toggle A --> */}
+          <div className="flex items-center justify-center w-full mt-4">
+            <label 
+              htmlFor="toogleA"
+              className="flex items-center cursor-pointer"
+            > 
+              {/* <!-- label --> */}
+              <div className={passphrase ? `text-gray-700 mr-3 font-medium text-sm` : `text-${color}-400 mr-3 font-medium text-sm`}>
+                P@$$w0rd!
+              </div>
+              {/* <!-- toggle --> */}
+              <div className="relative">
+                {/* <!-- input --> */}
+                <input id="toogleA" type="checkbox" className="sr-only" checked={passphrase} onChange={(event) => handlePassphrase(event.target.checked)}/>
+                {/* <!-- line --> */}
+                <div className="w-10 h-4 bg-gray-400 rounded-full shadow-inner"></div>
+                {/* <!-- dot --> */}
+                <div className="dot absolute w-6 h-6 bg-white rounded-full shadow -left-1 -top-1 transition"></div>
+              </div>
+              {/* <!-- label --> */}
+              <div className={!passphrase ? `text-gray-700 ml-3 font-medium text-sm` : `text-${color}-400 ml-3 font-medium text-sm`}>
+                correct-horse-battery-staple
+              </div>
+            </label>
+          </div>
           {/* CUSTOMISE PASSWORD COLUMNS */}
           <div className="flex flex-wrap mt-4">
             {/* LENGTH */}
@@ -227,34 +285,34 @@ function App() {
             {/* RADIOS */}
             <div className="flex flex-col flex-1 p-4 min-w-max">
               <label className="inline-flex items-center">
-                <input type="radio" value="say" checked={radio === "say"} onChange={handleRadio}/> 
+                <input type="radio" value="say" checked={radio === "say"} onChange={handleRadio} disabled={passphrase}/> 
                 <span className="ml-2 text-gray-800">Easy to say</span> 
               </label>
               <label className="inline-flex items-center">
-                <input type="radio" value="read" checked={radio === "read"} onChange={handleRadio}/>
+                <input type="radio" value="read" checked={radio === "read"} onChange={handleRadio} disabled={passphrase}/>
                 <span className="ml-2 text-gray-800">Easy to read</span>
               </label>
               <label className="inline-flex items-center">
-                <input type="radio" value="all" checked={radio === "all"} onChange={handleRadio}/>
+                <input type="radio" value="all" checked={radio === "all"} onChange={handleRadio} disabled={passphrase}/>
                 <span className="ml-2 text-gray-800">All characters</span>
               </label>
             </div>
             {/* CHECKBOXES */}
             <div className="flex flex-col flex-1 p-4 min-w-max">
               <label className="">
-                <input type="checkbox" name="Uppercase" checked={uppercase} onChange={(event) => setUppercase(event.target.checked)} /> 
+                <input type="checkbox" name="Uppercase" checked={uppercase} onChange={(event) => setUppercase(event.target.checked)} disabled={passphrase}/> 
                 <span className="ml-2 text-gray-800">Uppercase</span> 
               </label>
               <label className="">
-                <input type="checkbox" name="Lowercase" checked={lowercase} onChange={(event) => setLowercase(event.target.checked)} /> 
+                <input type="checkbox" name="Lowercase" checked={lowercase} onChange={(event) => setLowercase(event.target.checked)} disabled={passphrase}/> 
                 <span className="ml-2 text-gray-800">Lowercase</span>
               </label>
               <label className="">
-                <input type="checkbox" name="Numbers" checked={numbers} onChange={(event) => setNumbers(event.target.checked)} disabled={radio === 'say'}/> 
+                <input type="checkbox" name="Numbers" checked={numbers} onChange={(event) => setNumbers(event.target.checked)} disabled={radio === 'say' || passphrase}/> 
                 <span className="ml-2 text-gray-800">Numbers</span>
               </label>
               <label className="">
-                <input type="checkbox" name="Symbols" checked={symbols} onChange={(event) => setSymbols(event.target.checked)} disabled={radio === 'say'}/> 
+                <input type="checkbox" name="Symbols" checked={symbols} onChange={(event) => setSymbols(event.target.checked)} disabled={radio === 'say' || passphrase}/> 
                 <span className="ml-2 text-gray-800">Symbols</span>
               </label>
             </div>
