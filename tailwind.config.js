5731812c4c204795a4390ea05aafc5e0b9b7b7e6
Matthew Beardsley <matthew@beardsley.com.au>
2021-12-15 16:25:00 +0800

    broken

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,41 +1,36 @@
 module.exports = {
-  purge: 
-    {
-      content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-      safelist: [
-        'via-gray-400',
-        'via-yellow-400',
-        'via-green-400',
-        'via-red-400',
-        'bg-gray-400',
-        'bg-yellow-400',
-        'bg-green-400',
-        'bg-red-400',
-        'text-gray-500',
-        'text-yellow-500',
-        'text-green-500',
-        'text-red-500',
-        'text-gray-400',
-        'text-yellow-400',
-        'text-green-400',
-        'text-red-400',
-        'hover:text-gray-400',
-        'hover:text-yellow-400',
-        'hover:text-green-400',
-        'hover:text-red-400',
-        'border-gray-400',
-        'border-yellow-400',
-        'border-green-400',
-        'border-red-400',
-
-      ]
-    },
-  darkMode: false, // or 'media' or 'class'
+  purge: {
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    safelist: [
+      "via-gray-400",
+      "via-yellow-400",
+      "via-green-400",
+      "via-red-400",
+      "bg-gray-400",
+      "bg-yellow-400",
+      "bg-green-400",
+      "bg-red-400",
+      "text-gray-500",
+      "text-yellow-500",
+      "text-green-500",
+      "text-red-500",
+      "text-gray-400",
+      "text-yellow-400",
+      "text-green-400",
+      "text-red-400",
+      "hover:text-gray-400",
+      "hover:text-yellow-400",
+      "hover:text-green-400",
+      "hover:text-red-400",
+      "border-gray-400",
+      "border-yellow-400",
+      "border-green-400",
+      "border-red-400",
+    ],
+  },
+  content: [],
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
   plugins: [],
-}
+};
